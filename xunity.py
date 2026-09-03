ef3b881bc625a2908d9a3aeb807f86a5b542383b
thales05faria <thales05faria@gmail.com>
2026-09-02 22:27:06 -0300

    2. Classe TestResult

--- xunity.py
+++ xunity.py
@@ -1,10 +1,35 @@
+class TestResult:
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+    def test_started(self):
+        self.run_count += 1
+    def add_failure(self, test):
+        self.failures.append(test)
+    def add_error(self, test):
+        self.errors.append(test)
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}'
+
 class TestCase:
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
-    def run(self):
+    def run(self, result):
+        result.test_started()
         self.set_up()
-        test_method = getattr(self, self.test_method_name)
-        test_method()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
         self.tear_down()
     def set_up(self):
         pass
