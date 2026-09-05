0a6d6a2e8f9a6be4f02b154d33557ba9ebdb4dae
thales05faria <thales05faria@gmail.com>
2026-09-04 22:42:23 -0300

    6. Executando Todos os Testes

--- xunity.py
+++ xunity.py
@@ -178,3 +178,18 @@ class TestRunner:
         test.run(self.result)
         print(self.result.summary())
         return self.result
+
+
+if __name__ == "__main__":
+    loader = TestLoader()
+    test_case_suite = loader.make_suite(TestCaseTest)
+    test_suite_suite = loader.make_suite(TestSuiteTest)
+    test_load_suite = loader.make_suite(TestLoaderTest)
+
+    suite = TestSuite()
+    suite.add_test(test_case_suite)
+    suite.add_test(test_suite_suite)
+    suite.add_test(test_load_suite)
+
+    runner = TestRunner()
+    runner.run(suite)
