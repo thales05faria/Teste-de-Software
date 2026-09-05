539ac83443669ff3e95d607a75f7e03b7ecc3372
thales05faria <thales05faria@gmail.com>
2026-09-04 22:43:13 -0300

    7. Comandos Assert

--- xunity.py
+++ xunity.py
@@ -36,6 +36,23 @@ class TestCase:
     def tear_down(self):
         pass
 
+    def assert_equal(self, first, second):
+        if first != second:
+            msg = f'{first} != {second}'
+            raise AssertionError(msg)
+    def assert_true(self, expr):
+        if not expr:
+            msg = f'{expr} is not true'
+            raise AssertionError(msg)
+    def assert_false(self, expr):
+        if expr:
+            msg = f'{expr} is not false'
+            raise AssertionError(msg)
+    def assert_in(self, member, container):
+        if member not in container:
+            msg = f'{member} not found in {container}'
+            raise AssertionError(msg)
+
 class TestStub(TestCase):
     def test_success(self):
         assert True
@@ -100,6 +117,26 @@ class TestCaseTest(TestCase):
         spy = TestSpy('test_method')
         spy.run(self.result)
         assert spy.log == "set_up test_method tear_down"
+
+    def test_assert_true(self):
+        self.assert_true(True)
+    def test_assert_false(self):
+        self.assert_false(False)
+    def test_assert_equal(self):
+        self.assert_equal("", "")
+        self.assert_equal("foo", "foo")
+        self.assert_equal([], [])
+        self.assert_equal(['foo'], ['foo'])
+        self.assert_equal((), ())
+        self.assert_equal(('foo',), ('foo',))
+        self.assert_equal({}, {})
+        self.assert_equal({'foo'}, {'foo'})
+    def test_assert_in(self):
+        animals = {'monkey': 'banana', 'cow': 'grass', 'seal': 'fish'}
+        self.assert_in('a', 'abc')
+        self.assert_in('foo', ['foo'])
+        self.assert_in(1, [1, 2, 3])
+        self.assert_in('monkey', animals)
 
 class TestSuite:
     def __init__(self):
